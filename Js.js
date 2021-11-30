275741f2e8858dbbc9e68127474f1aea7e46c158
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-01 04:52:56 +0530

    Create Js.js

---
+++ Js.js
@@ -0,0 +1,4 @@
+<script language="javascript">
+function openNav() {document.getElementById("mySidenav").style.width="28%";}
+function closeNav() {document.getElementById("mySidenav").style.width="0%";}
+	</script>
